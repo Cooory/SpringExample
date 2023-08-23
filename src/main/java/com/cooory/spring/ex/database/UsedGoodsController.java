2f8b6ef7bf403301c7a8c9a09b99495480a800d7
Cory <cory1459@gmail.com>
2023-08-23 18:04:51 +0900

    0823

--- src/main/java/com/cooory/spring/ex/database/UsedGoodsController.java
+++ src/main/java/com/cooory/spring/ex/database/UsedGoodsController.java
@@ -1,16 +1,31 @@
 package com.cooory.spring.ex.database;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cooory.spring.ex.database.domain.UsedGoods;
+import com.cooory.spring.ex.database.service.UsedGoodsService;
 
 // request, response를 담당한다.
 @Controller
 public class UsedGoodsController {
+	
+	@Autowired
+	private UsedGoodsService usedGoodsService;
 
 	// used goods 데이터 조회 결과 json으로 response에 채운다.
 	@RequestMapping("/db/usedgoods/list")
-	public usedGoodsList() {
+	@ResponseBody
+	public List<UsedGoods> usedGoodsList() {
 		
+		List<UsedGoods> usedGoodsList = usedGoodsService.getUsedGoodsList();
+		
+		
+		return usedGoodsList;
 		
 	}
 }
--- src/main/java/com/cooory/spring/ex/database/service/UsedGoodsService.java
+++ src/main/java/com/cooory/spring/ex/database/service/UsedGoodsService.java
@@ -1,13 +1,26 @@
 package com.cooory.spring.ex.database.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cooory.spring.ex.database.domain.UsedGoods;
+import com.cooory.spring.ex.database.repository.UsedGoodsRepository;
 
 // 로직 (business) 담당
 @Service
 public class UsedGoodsService {
 	
+	@Autowired // 객체 관리를 스프링이 대신 해줌
+	private UsedGoodsRepository usedGoodsRepository;
+	
 	// used_goods 모든 데이터 조회
-	public getUsedGoodsList() {
+	public List<UsedGoods> getUsedGoodsList() {
+		
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		return usedGoodsList;
+		
 		
 	}
 }
