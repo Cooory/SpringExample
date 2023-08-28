95f0577072b06e332fe2426ced443fe8a2411b22
cooory <cory1459@gmail.com>
2023-08-28 18:49:00 +0900

    0828

---
+++ src/main/java/com/cooory/spring/ex/jsp/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.cooory.spring.ex.jsp.repository;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.cooory.spring.ex.jsp.domain.User;
+
+@Repository
+public interface UserRepository {
+
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email);
+	
+	public User selectLastUser();
+}
